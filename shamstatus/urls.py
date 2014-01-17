755ddc1dfca4c4b35db1a14c15c9d7d0be3e22a4
Marco DallaG <marco.dallagiacoma@gmail.com>
2014-01-17 11:08:11 +0100

    moved status page to homepage

--- shamstatus/urls.py
+++ shamstatus/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'shamstatus.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^test', check_global_status),
-    url(r'^status', status),
+    url(r'^$', status),
     url(r'^history', global_statuses_history),
 
     url(r'^admin/', include(admin.site.urls)),
